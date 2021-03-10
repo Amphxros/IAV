18c93c93b9cbaad15a423e4383366dd368770331
Amph <amprubio@ucm.es>
2021-03-10 12:34:21 +0100

    Añadida rata a la escena

--- Practica1/Assets/Scripts/Code/EvadeObstacles.cs
+++ Practica1/Assets/Scripts/Code/EvadeObstacles.cs
@@ -2,43 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace UCM.IAV.Movimiento 
-{
+
+namespace UCM.IAV.Movimiento{
     public class EvadeObstacles : ComportamientoAgente
     {
         public float look;
         public float distance;
         private Seguir seguir_; //seek al que cambiaremos el objetivo
-
+        private MerodeoCinematico merodeo_;
         void Start()
         {
             seguir_ = GetComponent<Seguir>();
+            merodeo_ = GetComponent<MerodeoCinematico>();
         }
 
-        //public override Direccion GetDireccion()
-        //{
-        //    Direction result = new Direction();
-        //    if (!seguir_.enabled)
-        //    {
-        //        return result;
-        //    }
-        //    else
-        //    {
-        //        Vector3 ray = agente.velocidad;
-        //        ray.Normalize();
-        //        ray *= look;
-
-        //        RaycastHit hit; //raycast
-
-        //        //si hay colision
-        //        if (Physics.Raycast(transform.position, ray, out hit))
-        //        {
-        //            result.lineal = hit.collider.gameObject.transform.position + hit.normal * distance;
-        //        }
-
-
-        //        return result;
-        //    }
-        // }
+      //  public override Direccion GetDireccion()
+      //  {
+      //      Direction result = new Direction();
+      //         Vector3 ray = agente.velocidad;
+      //          ray.Normalize();
+      //          ray *= look;
+      //
+      //          RaycastHit hit; //raycast
+      //
+      //          //si hay colision
+      //          if (Physics.Raycast(transform.position, ray, out hit))
+      //          {
+      //              result.lineal = hit.collider.gameObject.transform.position + hit.normal * distance;
+      //          }
+      //
+      //
+      //          return result;
+      //  }
     }
 }
