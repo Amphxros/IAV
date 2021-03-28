2afaec1e6338bcd6bdb1a0f7825be8812fb09af3
liyliLee <liyli@ucm.es>
2021-03-28 18:40:16 +0200

    -

---
+++ Practica2/Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public bool suavizado = false;
+
+    private Transform playerTr;
+    private float x, y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // x= input.GetAxis("Horizontal");
+        // y= input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LEFTARROW))
+            debug.log("Izq");
+        else if (Input.GetKeyDown(KeyCode.RIGHTARROW))
+            debug.log("der");
+        else if (Input.GetKeyDown(KeyCode.UPARROW))
+            debug.log("avan");
+        else if (Input.GetKeyDown(KeyCode.DOWNARROW))
+            debug.log("volver");
+    }
+}
