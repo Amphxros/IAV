832a9c42e158b505fd8d98feb23f3bb444dc9daf
Alejandro Placer <aplazer@ucm.es>
2021-03-28 20:00:28 +0200

    movimiento de jugador version 1.0

--- Practica2/Assets/Scripts/MovimientoJugador.cs
+++ Practica2/Assets/Scripts/MovimientoJugador.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
-    public bool suavizado = false;
-
     private Transform playerTr;
-    private float x, y;
-    // Start is called before the first frame update
+    private float timeInput = 0.5f;
+    private float gridCellSize = 1.0f;
+
     void Start()
     {
         playerTr = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // x= input.GetAxis("Horizontal");
-        // y= input.GetAxis("Vertical");
+        if (timeInput < 0.0f)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                playerTr.position = playerTr.position + new Vector3(-gridCellSize, 0, 0);
+                Debug.Log("Izq");
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                playerTr.position = playerTr.position + new Vector3(gridCellSize, 0, 0);
+                Debug.Log("der");
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                playerTr.position = playerTr.position + new Vector3(0, 0, gridCellSize);
+                Debug.Log("avan");
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                playerTr.position = playerTr.position + new Vector3(0, 0, -gridCellSize);
+                Debug.Log("volver");
+            }
+            timeInput = 0.5f;
+        }
+        else
+        {
+            timeInput -= Time.deltaTime;
+        }
+    }
 
-        if (Input.GetKeyDown(KeyCode.LEFTARROW))
-            debug.log("Izq");
-        else if (Input.GetKeyDown(KeyCode.RIGHTARROW))
-            debug.log("der");
-        else if (Input.GetKeyDown(KeyCode.UPARROW))
-            debug.log("avan");
-        else if (Input.GetKeyDown(KeyCode.DOWNARROW))
-            debug.log("volver");
+    public void SetGridCellSize(float size)
+    {
+        gridCellSize = size;
     }
 }
---
+++ Practica2/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private MovimientoJugador movJugador;
+    private MovimientoAutomatico movAuto;
+
+    // Esto lo deberia tener A*
+    public float tamCasilla = 1.0f;
+    
+    private float SpaceBarPressedTime = 0.0f;
+    public bool suavizado = false;
+
+    public GameObject salida;
+    private Graph grafo;
+    private List<Vertex> vertices;
+
+    private bool spaceBar = false;
+
+    // private componente que pinte la ruta de salida
+
+    void Start()
+    {
+        movJugador = GetComponent<MovimientoJugador>();
+        movAuto = GetComponent<MovimientoAutomatico>();
+        //grafo = GetComponent<Graph>();
+        
+        if (tamCasilla != 1.0f)
+        {
+            movJugador.SetGridCellSize(tamCasilla);
+            movAuto.SetGridCellSize(tamCasilla);
+        }
+    }
+
+    void Update()
+    {
+        // Podria ir en el scrip de A*
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            suavizado = !suavizado;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SpaceBarPressedTime += Time.deltaTime;
+            spaceBar = true;
+            Debug.Log("Clicando space bar");
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && spaceBar)
+        {
+            // Llamar a A* para que deje de dibujar
+            SpaceBarPressedTime = 0.0f;
+            spaceBar = false;
+            Debug.Log("Deja de dibujar");
+        }
+        if (SpaceBarPressedTime > 2.5f)
+        {
+            // Manda mensaje de control automatico
+            movAuto.enabled = true;
+            movJugador.enabled = false;
+            //movAuto.GetPathToExit(vertices);
+            Debug.Log("Cambiando a modo automatico");
+        }
+        else
+        {
+            // Mostrar la ruta del hilo
+            movAuto.enabled = false;
+            movJugador.enabled = true;
+            // Llamar al A* para que dibuje la ruta
+            //vertices = grafo.GetPath(gameObject, salida);
+            Debug.Log("Cambiando a modo manual");
+            Debug.Log("Dibuja el camino de A*");
+        }
+    }
+}
