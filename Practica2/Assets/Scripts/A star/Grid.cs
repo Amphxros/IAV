df572fb3daec286369b60d3324fcfae784534425
Amph <amprubio@ucm.es>
2021-03-29 12:14:57 +0200

    Update Grid.cs

--- Practica2/Assets/Scripts/A star/Grid.cs
+++ Practica2/Assets/Scripts/A star/Grid.cs
@@ -6,17 +6,50 @@ using UnityEngine;
 /// <summary>
 /// ESTRUCTURA DE NODOS
 /// </summary>
-public class Grid : MonoBehaviour
+public class Grid<T>
 {
-    // Start is called before the first frame update
-    void Start()
+    // Constants
+    private const int DEFAULT_SIZE = 4;
+    // Fields
+    private T[] _data = new T[DEFAULT_SIZE];
+    private int count_ = 0;
+    private int capacity_ = DEFAULT_SIZE;
+    private bool sorted_;
+
+    public int Count
     {
-        
+        get { return count_; }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int Capacity
     {
-        
+        get { return capacity_; }
+
+        set
+        {
+            int previousCapacity = _capacity;
+            capacity_ = Math.Max(value, _count);
+            if (capacity_ != previousCapacity)
+            {
+                T[] temp = new T[capacity_];
+                Array.Copy(_data, temp, count_);
+                _data = temp;
+            }
+        }
+
     }
+
+    public Grid()
+    {
+
+    }
+    public Grid(T[] data, int count)
+    {
+
+    }
+    public T Peek()
+    {
+        return _data[0];
+    }
+
 }
