b2315a22e9318e6ac9bc84bf990d8be9b3d20ffb
Alejandro Placer <aplazer@ucm.es>
2021-04-24 20:50:18 +0200

    cambios varios

---
+++ Practica3/Assets/Scripts/ComportamientoExpectador.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCM.IAV.Movimiento
+{
+    public class ComportamientoExpectador : MonoBehaviour
+    {
+        bool huir = false;
+
+        // Si una lampara se cae los expectadores huiran
+        public void LamparaCaida()
+        {
+            huir = true;
+        }
+
+        public void LamparaReparada()
+        {
+            huir = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Si estan huyendo, los expectadores saldran del palco al patio. Caso contrario regresaran
+            if (huir)
+            {
+                if (transform.position.z >= -25)
+                    transform.Translate(new Vector3(0, 0, -0.2f));
+            }
+            else
+            {
+                if (transform.position.z <= -12.7f)
+                {
+                    transform.Translate(new Vector3(0, 0, 0.2f));
+                }
+            }
+        }
+    }
+}
---
+++ Practica3/Assets/Scripts/Lampara.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCM.IAV.Movimiento
+{
+    public class Lampara : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private Rigidbody rb;
+        public bool lamparaCaida; //esta en su sitio
+        public Vector3 PosIni;
+        AudioSource sonido;
+
+        void Start()
+        {
+            lamparaCaida = false;
+            rb = GetComponent<Rigidbody>();
+            sonido = gameObject.GetComponent<AudioSource>();
+        }
+        public void Caida()
+        {
+            //Si la lampara cae, se le añade gravedad y hacemos que el publico se vaya
+            lamparaCaida = true;
+            rb.useGravity = true;
+        }
+        public void Subida()
+        {
+            //Ponemos la variable a true y ponemos la lampara en la posicion inicial
+            lamparaCaida = false;
+            rb.useGravity = false;
+            transform.position = PosIni;
+        }
+
+        private void Update()
+        {
+            //Para que las lamparas no se puedan mover cuando estan en el suelo
+            if (transform.position.y < 0.55f)
+                rb.isKinematic = true;
+            else
+                rb.isKinematic = false;
+        }
+    }
+}
