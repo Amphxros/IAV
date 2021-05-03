5d5e458ca00d37445181b9b52ac9d2abd7d5d49c
Amph <amprubio@ucm.es>
2021-05-03 20:53:50 +0200

    El jugador puede arreglar lamparas

---
+++ Practica3/Assets/Scripts/Interactuar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactuar : MonoBehaviour
+{
+    private bool isInteracting = false;
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            isInteracting = true;
+        }
+        else
+        {
+            isInteracting = false;
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        CaidaLampara c = col.gameObject.GetComponent<CaidaLampara>();
+
+        if (c != null)
+        {
+            if (isInteracting)
+            {
+                c.Restaurar();
+            }
+        }
+    }
+}
