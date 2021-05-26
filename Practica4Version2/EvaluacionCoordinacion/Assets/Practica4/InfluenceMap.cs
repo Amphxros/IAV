0bf2f703adfd725186b41ca2f114616bc41c5c94
Amph <amprubio@ucm.es>
2021-05-26 15:36:34 +0200

    Avances del mapa de influencia

--- Practica4Version2/EvaluacionCoordinacion/Assets/Practica4/InfluenceMap.cs
+++ Practica4Version2/EvaluacionCoordinacion/Assets/Practica4/InfluenceMap.cs
@@ -2,23 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InfluenceMap : MonoBehaviour
+namespace es.ucm.fdi.iav.rts
 {
-    public int dimensionX, dimensionZ;
-
-    private Vector3[,] points; 
-    // Start is called before the first frame update
-    void Start()
+    public class InfluenceMap : MonoBehaviour
     {
+        public int dimensionX, dimensionY, dimensionZ;
+        private Vector3[,] points; 
+
+        private List<BaseFacility> TeamAFacilities;
+        private List<ProcessingFacility> TeamAPFacilities;
+        private List<ExtractionUnit> TeamAUnitsExtractList;
+        private List<ExplorationUnit> TeamAUnitsExploreList;
+        private List<DestructionUnit> TeamAUnitsDestroyerList;
+
+        // Las listas completas de instalaciones y unidades del enemigo
+        private List<BaseFacility> TeamBFacilities;
+        private List<ProcessingFacility> TeamBPFacilities;
+        private List<ExtractionUnit> TeamBUnitsExtractList;
+        private List<ExplorationUnit> TeamBUnitsExploreList;
+        private List<DestructionUnit> TeamBUnitsDestroyerList;
+    
+        bool hasReferences_=false;
+        void Start()
+        {
+        int dimX= -dimensionX/2;
+        int dimZ= -dimensionZ/2;
+
         points = new Vector3[dimensionX, dimensionZ];
 
+        for(int i=0;i<dimensionX;i++){
+            for(int j=0;j<dimensionZ;j++){
+                points[i,j]=new Vector3(dimX + i, dimensionY, dimZ+ j);
+            }
+        }
 
+        }   
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        void Update()
+        {
+            if(!hasReferences_){
+            // pillar todas las referencias del GM
         
+
+            hasReferences_=true;
+            }
+        }
+
+        void UpdateNodes(){
+            if(!hasReferences_){
+                return;
+            }
+            else{
+                
+            }
+        }
     }
 }
